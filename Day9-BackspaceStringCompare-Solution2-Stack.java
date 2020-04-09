0831ab5f555f29f6fcff59fa6de48595219008d6
mishra28soumya <mishra28soumya@gmail.com>
2020-04-09 23:32:40 +0530

    Create Day9-BackspaceStringCompare-Solution2-Stack.java

---
+++ Day9-BackspaceStringCompare-Solution2-Stack.java
@@ -0,0 +1,62 @@
+/* Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+Example 1:
+
+Input: S = "ab#c", T = "ad#c"
+Output: true
+Explanation: Both S and T become "ac".
+Example 2:
+
+Input: S = "ab##", T = "c#d#"
+Output: true
+Explanation: Both S and T become "".
+Example 3:
+
+Input: S = "a##c", T = "#a#c"
+Output: true
+Explanation: Both S and T become "c".
+Example 4:
+
+Input: S = "a#c", T = "b"
+Output: false
+Explanation: S becomes "c" while T becomes "b".
+Note:
+
+1 <= S.length <= 200
+1 <= T.length <= 200
+S and T only contain lowercase letters and '#' characters. */
+
+
+
+class Solution {
+    public boolean backspaceCompare(String S, String T) {
+        
+        String s1 = removeBackspace(S);
+        String t1= removeBackspace(T);
+        if(s1.equals(t1)){
+            return true;
+        }
+        return false;
+    
+    }
+     public String removeBackspace(String s){
+         
+        Stack<Character> q = new Stack<Character>();
+        String res = "";
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)!='#'){
+                q.push(s.charAt(i));
+              
+            } else if(!q.isEmpty()){
+                q.pop();
+                
+            }
+        }
+         while(!q.isEmpty()){
+             res = res+q.pop();
+         }
+         return res;
+     }
+}
+
+
